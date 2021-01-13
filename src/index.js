107dfff6de7a519633c64a788f0236104dc969b0
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-13 12:50:22 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ClickityClick from './components/ClickityClick';
- 
+import LightSwitch from './components/LightSwitch'
 ReactDOM.render(
   <ClickityClick />,
+  <LightSwitch />,
   document.getElementById('root')
 );
