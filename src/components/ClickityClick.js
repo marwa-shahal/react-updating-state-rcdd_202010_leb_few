335a0198c0c30e9bf03fa9bc64685130516d7fd5
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-13 12:42:30 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -33,6 +33,13 @@ class ClickityClick extends React.Component {
 //   })
 //   console.log(this.state.hasBeenClicked); // prints false
 // }
+
+
+handleClick = () => {
+  this.setState({
+    hasBeenClicked: true
+  }, () => console.log(this.state.hasBeenClicked)) // prints true
+}
  
   render() {
     return (
